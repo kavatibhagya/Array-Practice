e4106ae02bf3b50b1bdf6612e939012ebcf7353a
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-03-01 10:26:56 -0800

    adding even or odd array

---
+++ javas.js
@@ -0,0 +1,19 @@
+var n;
+var even = [];
+ var odd = [];
+function evenOrOdd(n)
+{
+    for( var i=0; i<n; i++)
+    {
+        if(i%2==0)
+        {
+          even.push(i);
+        }
+        else {
+           odd.push(i);
+        }
+    }
+}
+evenOrOdd(10);
+console.log(even);
+console.log(odd);
